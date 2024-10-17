4de6b5aaa16121b734764b8c706928003c9e8731
Sun Sreng <sun_sreng@hotmail.com>
2024-10-17 15:52:46 +0700

    ok

--- Features/Program.cs
+++ Features/Program.cs
@@ -2,9 +2,10 @@
 // Input: "Hello World!"
 // Output: !dlroW olleH
 
-using Features.Extensions;
-using Features.Services;
+using Features.Helpers;
 
 string input = "Hello, WOrld!";
-Console.WriteLine($"Input: {input}, Output: {StringService.ReverseString(input)}");
-Console.WriteLine($"Input: {input}, Output: {input.Reverse()}");
+// Console.WriteLine($"Input: {input}, Output: {StringService.ReverseString(input)}");
+// Console.WriteLine($"Input: {input}, Output: {input.Reverse()}");
+var helper = new CustomStringHelper();
+Console.WriteLine(helper.Reverse(input));
